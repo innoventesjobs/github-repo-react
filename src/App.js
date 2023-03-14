1b55b75af6a9f6c579ad77e277777356e3af6551
V S SRINIDHI <76029867+SRINIDHI-VS@users.noreply.github.com>
2023-03-14 22:12:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [fork, setFork] = useState(false);
+  const [userobj, setUserobj] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!user) return;
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${user}/repos`
+        );
+        const data = await response.json();
+        setUserobj(data);
+      } catch (error) {
+        return <h1>Not Found</h1>;
+      }
+    }
+    fetchData();
+  };
+
   return (
     <div className="App">
       <div className="input">
-        <label htmlFor="username">Github username: </label>
-        <input id="username" type="text" />
-        <label htmlFor="fork">Include forks: </label>
-        <input id="fork" type="checkbox" />
-        <button>Submit</button>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Github username: </label>
+          <input
+            id="username"
+            type="text"
+            value={user}
+            onChange={(e) => {
+              setUser(e.target.value);
+            }}
+          />
+          <label htmlFor="fork">Include forks: </label>
+          <input
+            id="fork"
+            type="checkbox"
+            onChange={() => {
+              setFork(!fork);
+            }}
+          />
+          <button type="submit" disabled={!user}>
+            Submit
+          </button>
+        </form>
       </div>
       <section>
         <header>
@@ -18,14 +57,27 @@ function App() {
           <div className="col">Description</div>
           <div className="col">Size</div>
         </header>
-        <div>
-          <div className="col">C1</div>
-          <div className="col">C2</div>
-          <div className="col">C3</div>
-          <div className="col">C4</div>
-        </div>
+
+        {userobj &&
+          userobj
+            .filter((item) => item.fork === fork)
+            .map((item) => {
+              return (
+                <div key={item.id}>
+                  <div className="col">{item.name}</div>
+                  <div className="col">{item.language}</div>
+                  <div className="col">{item.description}</div>
+                  <div className="col">{item.size}</div>
+                </div>
+              );
+            })
+            .sort((a, b) => {
+              return (
+                b.props.children[3].props.children -
+                a.props.children[3].props.children
+              );
+            })}
       </section>
-      <div className="error">Dummy error</div>
     </div>
   );
 }
